81b9d5641c3ea353cfeda7b170bd6a704114e7d1
juriki <juritokvin@gmailcom>
2022-11-15 09:24:06 +0200

    Initial Commit

---
+++ NuorisoKortti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NuorisoKortti
+//
+//  Created by Jurij Tokvin on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
