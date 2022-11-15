759492eb6b66d8fbb3075789e9f98922b24aa726
juriki <juritokvin@gmailcom>
2022-11-15 14:02:20 +0200

    Application Runko Valmis

--- NuorisoKortti/ViewController.swift
+++ NuorisoKortti/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var iloinenAskolaa: UIImageView!
+    
+
+    
+    
+    func kirjaudu(img: UIImageView, button: UIButton)
+    {
+        if button.currentTitle == "Kirjaudu"
+        {
+            button.setTitle("Ulos", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.7168534398, green: 0.06697402149, blue: 0.2032336891, alpha: 1)
+            img.isHidden = false
+        }
+        else
+        {
+            button.setTitle("Kirjaudu", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.2070960402, green: 0.7793012261, blue: 0.3491066098, alpha: 1)
+            img.isHidden = true
+            
+        }
     }
-
-
+    
+    @IBAction func Kirjaudu(_ sender: UIButton)
+    {
+        kirjaudu(img: iloinenAskolaa, button: sender)
+    }
+    
 }
 
